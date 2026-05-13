3d3defe1bc1a9f29cc7501d8d9c55b6aebc99888
Simon Du Plooy <47170836+SimonDuPlooyGit@users.noreply.github.com>
2026-05-13 14:59:58 +0200

    Ghost of movement

--- Assets/Scripts/PrototypeUnit.cs
+++ Assets/Scripts/PrototypeUnit.cs
@@ -9,17 +9,31 @@ using Vector3 = UnityEngine.Vector3;
 public class PrototypeUnit : MonoBehaviour
 {
     private InputActions input;
-    NavMeshAgent agent;
+    private NavMeshAgent agentUnit;
+    private NavMeshAgent agentGhost;
     public float movementStat;
     private float pathDistance;
     public NavMeshPath path;
+    public GameObject unitGhost;
+    public LineRenderer lineRenderer;
+    private Vector3[] points;
+    private bool pathDrawn = false;
 
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
+        agentUnit = GetComponent<NavMeshAgent>();
+        agentGhost = unitGhost.GetComponent<NavMeshAgent>();
         input = new InputActions();
         AssignInputs();
         path = new NavMeshPath();
+    }
+
+    private void Update()
+    {
+         if (agentGhost.remainingDistance == 0)
+         {
+             DrawPath(points);
+         }
     }
 
     void AssignInputs()
@@ -30,11 +44,13 @@ public class PrototypeUnit : MonoBehaviour
     void ClickToPathfind()
     {
         pathDistance = 0f;
+        lineRenderer.enabled = false;
+        pathDrawn = false;
         
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out hit, 100))
         {
-            if (NavMesh.CalculatePath(transform.position, hit.point, agent.areaMask, path))
+            if (NavMesh.CalculatePath(transform.position, hit.point, agentUnit.areaMask, path))
             {
                 for (int i = 0; i < path.corners.Length - 1; i++)
                 {
@@ -46,14 +62,31 @@ public class PrototypeUnit : MonoBehaviour
 
             if (pathDistance > movementStat)
             {
-                agent.destination = LimitPath(path, movementStat);
+                agentGhost.destination = LimitPath(path, movementStat);
             }
             else
             {
-               agent.destination = hit.point;
+               agentGhost.destination = hit.point;
             }
-            
-            //agent.destination = hit.point;
+
+            points = path.corners;
+            //DrawPath(points);
+
+        }
+    }
+
+    private void DrawPath(Vector3[] points)
+    {
+        if (pathDrawn == false)
+        {
+            Vector3 upOffset = new  Vector3(0f, 1f, 0f);
+            for (int i = 0; i < points.Length; i++)
+            {
+                //points[i] += upOffset;
+            }
+            lineRenderer.SetPositions(points);
+            lineRenderer.enabled = true;
+            pathDrawn = true;
         }
     }
     
