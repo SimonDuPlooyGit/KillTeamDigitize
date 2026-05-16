68ada0a7d032927a48810aa94e7a87d7ae81cfc6
Simon Du Plooy <47170836+SimonDuPlooyGit@users.noreply.github.com>
2026-05-16 14:19:07 +0200

    All NPO bases in

---
+++ Assets/Scripts/OperativeTemplate.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OperativeTemplate", menuName = "Scriptable Objects/OperativeTemplate")]
+public class OperativeTemplate : ScriptableObject
+{
+    //Stats
+    public int APL, MOVE, SAVE, WOUNDS;
+    
+    //Keywords such as Imperium, Astartes, Warrior etc
+    
+    //Possibly has active abilities you need to pay APL for
+    
+    //Possibly has passive effects that have different conditions to trigger such as ignoring saturate
+    
+    //Loadout of weapons
+}
+
+[System.Flags]
+public enum Keywords
+{
+    None = 0,
+    Imperium = 1 << 0,
+    AdeptusAstartes = 1 << 1,
+    Leader = 1 << 2,
+    SpaceMarineCaptain = 1 << 3,
+    AngelOfDeath = 1 << 4,
+    AssaultIntercessor = 1 << 5,
+    Sergeant = 1 << 6,
+    Intercessor = 1 << 7,
+    Grenadier = 1 << 8,
+    Warrior = 1 << 9,
+    HeavyIntercessor = 1 << 10,
+    Gunnner = 1 << 11,
+    Eliminator = 1 << 12,
+    Sniper = 1 << 13,
+}
+
--- Assets/Scripts/WeaponTemplate.cs
+++ Assets/Scripts/WeaponTemplate.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "Scriptable Objects/Weapon")]
 public class WeaponTemplate : ScriptableObject
 {
+    //Stats
     public int ATK, HIT, DMGnorm, DMGcrit;
+    
+    //Rules
     [SerializeReference, SubclassSelector]
     public List<IWeaponRule> rules = new List<IWeaponRule>();
 }
