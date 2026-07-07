73b7418a2863ed3d25f6548cf7168dcb27ac03c9
JesseVolstruis <2597626@students.wits.ac.za>
2026-07-07 18:58:22 +0200

    added basic dice rolling functionality

---
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic; 
+
+public class CombatManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject dicePrefab;
+    [SerializeField]
+    GameObject diceHolder;
+    private List<CombatRoll> activeDice = new List<CombatRoll>(); //List of rolled dice to track roll results
+    public List<CombatRoll> GetActiveDice() //use combatManager.GetActiveDice() to HOPEFULLY access the list in other scripts. Contact me(Joss) in case of emergency
+    {
+        return activeDice;
+    }
+    void Start()
+    {
+        
+    }
+
+
+    public void SpawnDice(int numRolled)
+    {
+        activeDice.Clear();
+        foreach(Transform child in diceHolder.transform) //iterates through children to clear them
+        {
+            GameObject childObj = child.gameObject;
+            Destroy(childObj);
+        }
+        for(int i=0; i<numRolled; i++) //rolls a certain number of dice
+        {
+            GameObject rolledDice = Instantiate(dicePrefab, diceHolder.transform);
+            CombatRoll rollScript = rolledDice.GetComponent<CombatRoll>(); //updates the list
+            if(rollScript != null )
+            {
+                activeDice.Add(rollScript);
+            }
+        }
+
+        HitTester();
+       
+    }
+
+    public void HitTester() //test that says hit when a die is above or equal to 3
+    {
+        foreach(CombatRoll die in activeDice)
+        {
+            int roll = die.rollResult;
+            if(roll >= 3 )
+            {
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+   
+}
--- Assets/Scripts/Combat/CombatRoll.cs
+++ Assets/Scripts/Combat/CombatRoll.cs
@@ -5,28 +5,39 @@ using UnityEngine.UI;
 public class CombatRoll : MonoBehaviour
 {
     [SerializeField]
-    Sprite[] diceSprites;
-    private GameObject dicePanel;
+    Sprite[] diceSprites; 
+    private GameObject dicePanel; //Child gameobject which is just the sprite
+    private Animator diceAnimator;
     [SerializeField]
-    float flipDelay = 0.001f;
-    void Start()
+    float cycleDelay = 2f; //The delay from the start of the animation to the roll result
+    public int rollResult; //the result of a roll. public for access
+
+    private void Awake()
     {
         dicePanel = transform.Find("Dice").gameObject;
-        
-        
+        diceAnimator = dicePanel.GetComponent<Animator>();
+        RollDice();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Rolls a random number, saves the result to rollResult, and starts the rolling animation
+    public void RollDice()
     {
-        
-    }
-
-    private void rollDice()
-    {
-        int randomRoll = Random.Range(0, diceSprites.Length);
+        int randomRoll = Random.Range(1, diceSprites.Length+1);
+        rollResult = randomRoll;
+        StartCoroutine(CycleDice(randomRoll));
+      
     }
     
-   
+    //rolling animation
+    private IEnumerator CycleDice(int rand)
+    {
+        diceAnimator.enabled = true;
+        diceAnimator.SetTrigger("Roll");
+        yield return new WaitForSeconds(cycleDelay);
+        diceAnimator.Play("Idle");
+        diceAnimator.enabled = false;
+        dicePanel.GetComponent<Image>().sprite = diceSprites[rand-1];
+
+    }
     
 }
