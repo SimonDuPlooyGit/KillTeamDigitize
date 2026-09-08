c7af902a555e98c0583313427434a5f6e25ffed6
JesseVolstruis <2597626@students.wits.ac.za>
2026-09-08 19:38:48 +0200

    Added standard reroll function

--- Assets/Scripts/Combat/DiceHandler.cs
+++ Assets/Scripts/Combat/DiceHandler.cs
@@ -8,6 +8,7 @@ using UnityEditor.Rendering.LookDev; //need this to access the Image component
 
 public class DiceHandler : MonoBehaviour
 {
+    //Determines which dice are allowed to be selected for rerolls
     public enum DiceSelectionMode
     {
         Attack,
@@ -32,16 +33,17 @@ public class DiceHandler : MonoBehaviour
     GameObject defenseDiceHolder; //The horizontal layout group for the defense dice prefabs
     [SerializeField]
     Transform diceThrowPoint; //Where dice spawn from
-    public List<CombatRoll> activeAttackDice = new List<CombatRoll>(); //List of rolled attack dice to track roll results
-    public List<CombatRoll> activeDefenseDice = new List<CombatRoll>(); //List of rolled defence dice
+    public List<CombatRoll> activeAttackDice = new List<CombatRoll>(); //List of rolled UI attack dice to track roll results for menu
+    public List<CombatRoll> activeDefenseDice = new List<CombatRoll>(); //List of rolled UI defence dice for menu
     [SerializeField]
     private Image healthFill; 
     [SerializeField]
     private float currentHealthTest;
     [SerializeField]
     private MenuPanel menu;
-    //List used to pass attack roll values in throwDefenseDice to populate the menu
-    public List<DiceRoll> tempPhysAttackDice = new List<DiceRoll>();
+    //Lists used to keep global track of attack and defense dice for rerolling purposes
+    public List<DiceRoll> activePhysAttackDice = new List<DiceRoll>();
+    public List<DiceRoll> activePhysDefenseDice = new List<DiceRoll>();
     //max amount of dice that can be selected to be rerolled, number will have to be set externally
     public int maxAllowedSelections = 2;
     public DiceSelectionMode currentSelectionMode = DiceSelectionMode.Attack; // Default to only select attack dice
@@ -88,7 +90,7 @@ public class DiceHandler : MonoBehaviour
     public IEnumerator ThrowAttackDice(int numDice, bool isAlly, InformationPackage context)
     {
         //clear tempPhysAttackDice to keep track of attack dice. this is used to populate the dice roll menu in throwDefenseDice coroutine
-        tempPhysAttackDice.Clear();
+        activePhysAttackDice.Clear();
         this.context = context;
         
         List<DiceRoll> thrownDice= new List<DiceRoll>();
@@ -102,7 +104,7 @@ public class DiceHandler : MonoBehaviour
             GameObject physAtkDie = Instantiate(AtkDiceObj, diceThrowPoint.position, Random.rotation);
             DiceRoll dieScript = physAtkDie.GetComponent<DiceRoll>();
             thrownDice.Add(dieScript);
-            tempPhysAttackDice.Add(dieScript);
+            activePhysAttackDice.Add(dieScript);
             yield return new WaitForSeconds(0.1f);
         }
 
@@ -136,7 +138,6 @@ public class DiceHandler : MonoBehaviour
         yield return new WaitForSeconds(2f);
         menu.OpenMenu(menu.diceRollMenu);
         SpawnDice(thrownDice, true, isAlly);
-        //yield return new WaitForSeconds(3);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         menu.CloseMenu(menu.diceRollMenu);
 
@@ -146,14 +147,15 @@ public class DiceHandler : MonoBehaviour
             Destroy(die.gameObject);
         }*/
     }
-    //==================[End coroutine]=======================
+    //==================[End throw attack coroutine]=======================
     
     //======================[Throws DEF Dice Physically]==========================================
     public IEnumerator ThrowDefenseDice(int numDice, bool isAlly, InformationPackage context) 
     {
+        activePhysDefenseDice.Clear();
         this.context = context;
         
-        List<DiceRoll> thrownDefDice = new List<DiceRoll>();
+        //List<DiceRoll> thrownDefDice = new List<DiceRoll>();
 
         //Assign prefab based on whether the attacker is an enemy or ally 
         GameObject DefDiceObj = !isAlly ? allyDicePhysical : enemyDicePhysical;
@@ -163,7 +165,7 @@ public class DiceHandler : MonoBehaviour
         {
             GameObject physDefDie = Instantiate(DefDiceObj, diceThrowPoint.position, Random.rotation);
             DiceRoll dieScript = physDefDie.GetComponent<DiceRoll>();
-            thrownDefDice.Add(dieScript);
+            activePhysDefenseDice.Add(dieScript);
             yield return new WaitForSeconds(0.1f);
         }
 
@@ -175,7 +177,7 @@ public class DiceHandler : MonoBehaviour
         {
             allStopped = true;
 
-            foreach(DiceRoll die in thrownDefDice)
+            foreach(DiceRoll die in activePhysDefenseDice)
             {
                 if(!die.IsStopped())
                 {
@@ -187,7 +189,7 @@ public class DiceHandler : MonoBehaviour
         }
 
         //Process Defense dice values
-        foreach (DiceRoll die in thrownDefDice)
+        foreach (DiceRoll die in activePhysDefenseDice)
         {
             int face = die.GetUpwardFace();
             context.defenseRolls.Add(face);
@@ -196,32 +198,98 @@ public class DiceHandler : MonoBehaviour
         //Populate UI panel with roll results
         yield return new WaitForSeconds(2f);
         menu.OpenMenu(menu.diceRollMenu);
-        SpawnDice(tempPhysAttackDice, true, isAlly);
-        SpawnDice(thrownDefDice, false, !isAlly);
-        //yield return new WaitForSeconds(3);
+        SpawnDice(activePhysAttackDice, true, isAlly);
+        SpawnDice(activePhysDefenseDice, false, !isAlly);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         menu.CloseMenu(menu.diceRollMenu);
-        
-        /*foreach (DiceRoll die in thrownDefDice)
-        {
-            Destroy(die.gameObject);
-        }*/
     }
-    //==================[End coroutine]=======================
+    //==================[End throw defense coroutine]=======================
 
-    /*public IEnumerator Reroll(int position, bool isAttack)
+    //======================[Standard Dice reroll]==========================================
+    public IEnumerator Reroll(bool isAlly)
     {
-        if (isAttack)
-        {
-            //context.attackRolls[position].reroll;
-        }
-        else
-        {
-            //context.defenseRolls[position].reroll;
-        }
-    }*/
+        //determines which prefabs and dice pool are used based on the current selection state
+        bool isAttackMode = (currentSelectionMode == DiceSelectionMode.Attack);
 
-    //Clears both dice roll result lists. 
+        List<CombatRoll> activeUIList = isAttackMode ? activeAttackDice : activeDefenseDice;
+        List<DiceRoll> activePhysList = isAttackMode ? activePhysAttackDice : activePhysDefenseDice;
+        List<int> activeContextList = isAttackMode ? context.attackRolls : context.defenseRolls;
+
+        GameObject diceObjPrefab = isAttackMode? (isAlly ? allyDicePhysical : enemyDicePhysical): (!isAlly ? allyDicePhysical : enemyDicePhysical);
+
+        //check which panels are selected by creating new list and comparing it to current list of panels
+        List<int> selectedDice = new List<int>();
+        for (int i = 0; i < activeUIList.Count; i++)
+        {
+            if (activeUIList[i] != null && activeUIList[i].IsSelected)
+            {
+                selectedDice.Add(i);
+            }
+        }
+        //case for if no dice are selected
+        if (selectedDice.Count == 0)
+        {
+            Debug.Log("[DiceHandler] No dice selected for reroll.");
+            yield break;
+        }
+
+        //Destroy corresponding physical dice
+        foreach (int index in selectedDice)
+        {
+            if (activePhysList[index] != null)
+            {
+                Destroy(activePhysList[index].gameObject);
+            }
+        }
+        //Close menu
+        menu.CloseMenu(menu.diceRollMenu);
+        yield return new WaitForSeconds(0.5f);
+
+        //Instantiate and throw new dice
+        List<DiceRoll> newPhysDice = new List<DiceRoll>();
+        for (int i = 0; i < selectedDice.Count; i++)
+        {
+            GameObject newPhysDie = Instantiate(diceObjPrefab, diceThrowPoint.position, Random.rotation);
+            DiceRoll dieScript = newPhysDie.GetComponent<DiceRoll>();
+            newPhysDice.Add(dieScript);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        //Wait for all new dice to stop moving
+        yield return new WaitForSeconds(0.5f);
+        bool allStopped = false;
+        while (!allStopped)
+        {
+            allStopped = true;
+            foreach (DiceRoll die in newPhysDice)
+            {
+                if (!die.IsStopped())
+                {
+                    allStopped = false;
+                    break;
+                }
+            }
+            yield return null;
+        }
+
+        //Replace updated indices in tracking lists and context
+        for (int i = 0; i < selectedDice.Count; i++)
+        {
+            int slotIndex = selectedDice[i];
+            DiceRoll newDie = newPhysDice[i];
+            int newFace = newDie.GetUpwardFace();
+
+            activePhysList[slotIndex] = newDie;
+            activeContextList[slotIndex] = newFace;
+        }
+        //Update the menu
+        menu.OpenMenu(menu.diceRollMenu);
+        bool isUIAlly = isAttackMode ? isAlly : !isAlly;
+        SpawnDice(activePhysList, isAttackMode, isUIAlly);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        menu.CloseMenu(menu.diceRollMenu);
+    }
+    //==================[End standard reroll coroutine]=======================
 
     //determines if a dice panel can be selected
     public bool CanSelectDie(bool isAttack)
@@ -246,11 +314,26 @@ public class DiceHandler : MonoBehaviour
 
         return totalSelected < maxAllowedSelections;
     }
+
     public void ClearDiceRolls()
     {
         currentSelectionMode = DiceSelectionMode.Attack;
         context.attackRolls.Clear();
         context.defenseRolls.Clear();
+        foreach(DiceRoll die in activePhysAttackDice)
+        {
+            Destroy(die.gameObject);
+        }
+
+        foreach (DiceRoll die in activePhysDefenseDice)
+        {
+            Destroy(die.gameObject);
+        }
     }
-    
+
+    public void RerollButton(bool isAlly)
+    {
+        StartCoroutine(Reroll(isAlly));
+    }
+
 }
